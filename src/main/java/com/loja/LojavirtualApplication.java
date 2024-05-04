52f4d8f80ae435f5c993dbeb5b99c59ec56433ff
Lourenco <Lourenco@Lourenco-Notebook>
2024-05-03 22:50:23 -0300

    primeira tabela criada

--- src/main/java/com/loja/LojavirtualApplication.java
+++ src/main/java/com/loja/LojavirtualApplication.java
@@ -2,7 +2,9 @@ package com.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.loja.model")
 @SpringBootApplication
 public class LojavirtualApplication {
 
